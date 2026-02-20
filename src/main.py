987f81046a76e8980448313494ac3d9b334b9b27
revcutter <brianxing1112@gmail.com>
2026-02-20 00:08:25 -0500

    B: hosting by GitHub Pages

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+import sys
 from pathlib import Path
 from textnode import TextType, TextNode
 from block_markdown import markdown_to_html_node
@@ -26,7 +27,7 @@ def initialize_public_files(dest_dir_path):
         shutil.rmtree(dest_dir_path)
     os.mkdir(dest_dir_path)
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     os.makedirs(os.path.dirname(dest_path), exist_ok=True)
     with open(from_path, 'r') as f:
@@ -36,10 +37,11 @@ def generate_page(from_path, template_path, dest_path):
     with open(template_path, 'r') as g:
         template = g.read()
         full_html = template.replace("{{ Title }}", title).replace("{{ Content }}", f_html)
+        full_html = full_html.replace('href="/', f'href="{basepath}').replace('src="/', f'src="{basepath}')
     with open(dest_path, 'w') as h:
         h.write(full_html)
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     if os.path.exists(dir_path_content):
         content_list = os.listdir(dir_path_content)
         for item in content_list:
@@ -47,12 +49,12 @@ def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
             if os.path.isfile(item_path):
                 new_dest_dir_path = os.path.join(dest_dir_path, item)
                 new_dest_html_path = Path(new_dest_dir_path).with_suffix(".html")
-                generate_page(item_path, template_path, new_dest_html_path)
+                generate_page(item_path, template_path, new_dest_html_path, basepath)
             else:
                 new_dest_dir_path = os.path.join(dest_dir_path, item)
                 os.makedirs(new_dest_dir_path, exist_ok=True)
                 print(f"Creating new {new_dest_dir_path} subdirectory at destination")
-                generate_pages_recursive(item_path, template_path, new_dest_dir_path)
+                generate_pages_recursive(item_path, template_path, new_dest_dir_path, basepath)
 
 
 def extract_title(markdown):
@@ -65,9 +67,13 @@ def extract_title(markdown):
             raise Exception("No heading in Markdown format found")
 
 def main():
-    initialize_public_files("public")
-    copy_files_recursive("static", "public")
-    generate_pages_recursive("content", "template.html", "public")
+    if len(sys.argv) > 1:
+        basepath = sys.argv[1]
+    else:
+        basepath = "/"
+    initialize_public_files("docs")
+    copy_files_recursive("static", "docs")
+    generate_pages_recursive("content", "template.html", "docs", basepath)
 
 main()
 
